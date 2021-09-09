48764bdc44b88e58231fc3e6886d4d0bb2c95dbe
Anwesh43 <anweshthecool0@gmail.com>
2021-09-09 15:21:02 +0530

    adding AroowLineRot class

--- index.ts
+++ index.ts
@@ -179,3 +179,26 @@ class ALRNode {
         return this 
     }
 }
+
+class ArrowLineRot {
+
+    curr : ALRNode = new ALRNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
