83d916ef738ad4514cae73885fa343626144547b
Anwesh43 <anweshthecool0@gmail.com>
2021-09-09 14:40:18 +0530

    adding DraawinguUtil class

--- index.ts
+++ index.ts
@@ -24,3 +24,42 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawArrowLineRot(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        context.save()
+        context.translate(w / 2, h / 2)
+        for (var j = 0; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1 - 2 * j)
+            context.translate(-size * 2, -(h + size) * sc3)
+            for (var k = 0; k < 2; k++) {
+                context.save()
+                context.rotate((sc2) * Math.PI * 0.25 * (1 - 2 * k))
+                DrawingUtil.drawLine(context, 0, 0, 0, -size * sc1)
+                context.restore()
+            }
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawALRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        DrawingUtil.drawArrowLineRot(context, scale)
+    }
+}
